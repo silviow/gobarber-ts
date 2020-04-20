7137dd01ffeee6272a8bba4c9ea6cfb6eb9745a2
Silvio Marques <silvio.mms2016@gmail.com>
2020-04-20 11:34:16 -0300

    Configuring Postgres with TypeORM and refactoring appointment routes for new data persistence

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import './database';
 
 const app = express();
 
--- backend/src/services/CreateAppointmentService.ts
+++ backend/src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,16 +9,14 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+        const appointmentsRepository = getCustomRepository(
+            AppointmentsRepository,
+        );
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
-
-    public execute({ provider, date }: RequestDTO): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(
             appointmentDate,
         );
 
@@ -25,10 +24,12 @@ class CreateAppointmentService {
             throw Error('This time is already booked.');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate,
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
