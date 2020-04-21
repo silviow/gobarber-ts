59ab940fd060f325dee165c0b290c35da6d244c9
Silvio Marques <silvio.mms2016@gmail.com>
2020-04-21 09:59:32 -0300

    Creating user model and migration, and relationship between users and appointments

---
+++ backend/src/database/migrations/1587473068523-CreatingAppointmentsTable.ts
@@ -0,0 +1,63 @@
+import {
+    MigrationInterface,
+    QueryRunner,
+    Table,
+    TableForeignKey,
+} from 'typeorm';
+
+export default class CreatingAppointmentsTable1587473068523
+implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointments',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                        generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
+                    },
+                    {
+                        name: 'provider_id',
+                        type: 'uuid',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'date',
+                        type: 'timestamp with time zone',
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()',
+                    },
+                ],
+            }),
+        );
+
+        await queryRunner.createForeignKey(
+            'appointments',
+            new TableForeignKey({
+                name: 'AppointmentProvider',
+                columnNames: ['provider_id'],
+                referencedColumnNames: ['id'],
+                referencedTableName: 'users',
+                onDelete: 'SET NULL',
+                onUpdate: 'CASCADE',
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+
+        await queryRunner.dropTable('appointments');
+    }
+}
