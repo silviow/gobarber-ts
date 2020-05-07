c032488dd2cf3230aa8b223cfecb3da959d5db62
Silvio Marques <silvio.mms2016@gmail.com>
2020-05-06 22:46:50 -0300

    Implementing cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -8,6 +9,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
